80b7c75305e7061778895d69c5a186c59b7f2ac4
Vitalik <vitalikdrobot1996@gmail.com>
2016-12-11 16:39:40 +0200

    Created habtm association

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,48 @@
+class TagsController < ApplicationController
+
+	before_action :current_tag, only: [:show, :edit, :update, :destroy]
+
+	def index
+    @tags = Tag.all
+	end
+
+	def new
+    @tag = Tag.new
+	end
+
+	def show 
+	end
+
+	def edit 
+	end
+
+	def create
+    tag = Tag.new(tag_params)
+    if tag.save
+      redirect_to tag
+    else
+      render :new
+    end
+	end
+
+  def update
+    @tag.update(tag_params)
+    redirect_to @tag
+  end
+
+  def destroy 
+    @tag.delete
+    redirect_to tags_path
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
+  def current_tag
+    @tag = Tag.find(params[:id])
+  end
+
+end
